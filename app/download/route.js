c094b5eacb56c8c8af05690ce34e65f04f020a09
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-11-30 18:55:44 +0530

    Update route.js

--- app/download/route.js
+++ app/download/route.js
@@ -1,8 +1,10 @@
+export const runtime = "nodejs";
+
 export async function POST(req) {
   try {
     const { downloadUrl } = await req.json();
     return Response.redirect(downloadUrl, 302);
-  } catch (err) {
-    return Response.json({ error: "Download error" }, { status: 400 });
+  } catch (e) {
+    return Response.json({ error: "Download error" }, { status: 500 });
   }
 }
